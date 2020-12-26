ac9e59d32618c9372df00cb18aaa11d8a9afdcc7
Victor-Trinh <trinhvictor12@gmail.com>
2020-12-25 23:45:01 -0500

    cleaned up handling when tabs are updated (but not closed)

--- background.js
+++ background.js
@@ -4,28 +4,28 @@ function getBaseUrl(url){
   return url.split("/")[2]
 }
 
-chrome.tabs.onUpdated.addListener(function(command) {
-  chrome.tabs.query({}, function (tabs) {
+chrome.tabs.onUpdated.addListener(function(updatedTabId) {
+  chrome.tabs.get(updatedTabId, function (updatedTab) {
     console.log(mutedTabs.keys());
-    for (var i = 0; i < tabs.length; i++) {
-      var audible = tabs[i].audible
-      var notMuted = tabs[i].mutedInfo;
-    
-      if (notMuted && audible){ 
-        console.log("Muting tab: ".concat(tabs[i].title));
-        chrome.tabs.update(tabs[i].id, {"muted": true});
-        mutedTabs.set(tabs[i].id, getBaseUrl(tabs[i].url))
-      } // muted tab has changed to new base url 
-      else if (mutedTabs.has(tabs[i].id) && (mutedTabs.get(tabs[i].id) !== getBaseUrl(tabs[i].url))) { 
-        if (audible) { 
-          console.log("Muting tab: ".concat(tabs[i].title));
-          mutedTabs.set(tabs[i].id, getBaseUrl(tabs[i].url))
-        } else { // unmute and delete from map 
-          console.log("unmuting tab: " + tabs[i].id);
-          chrome.tabs.update(tabs[i].id, {"muted": false});
-          console.log(mutedTabs.delete(tabs[i].id));
-        }
+    console.log(updatedTab);
+    var audible = updatedTab.audible;
+    var notMuted = updatedTab.mutedInfo;
+  
+    if (notMuted && audible){ 
+      console.log("Muting tab: ".concat(updatedTab.title));
+      chrome.tabs.update(updatedTab.id, {"muted": true});
+      mutedTabs.set(updatedTab.id, getBaseUrl(updatedTab.url));
+    } // muted tab has changed to new base url  
+    else if (mutedTabs.has(updatedTab.id) && (mutedTabs.get(updatedTab.id) !== getBaseUrl(updatedTab.url))) { 
+      if (audible) { 
+        console.log("Muting tab: ".concat(updatedTab.title));
+        mutedTabs.set(updatedTab.id, getBaseUrl(updatedTab.url));
+      } else { // unmute and delete from map 
+        console.log("unmuting tab: " + updatedTab.id);
+        chrome.tabs.update(updatedTab.id, {"muted": false});
+        console.log(mutedTabs.delete(updatedTab.id));
       }
+      
     }
   });
 });
