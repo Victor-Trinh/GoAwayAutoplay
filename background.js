8be54d38a45be7971a91cf111b4fb35f6e4526ec
Victor-Trinh <trinhvictor12@gmail.com>
2020-12-28 00:44:03 -0500

    integrated trusted sites list into main functionality

--- background.js
+++ background.js
@@ -5,28 +5,28 @@ function getBaseUrl(url){
 var mutedTabs = new Map() // tabs muted by the extension. tab id -> base url of tab 
 
 chrome.tabs.onUpdated.addListener(function(updatedTabId) {
-  chrome.tabs.get(updatedTabId, function (updatedTab) {
-    console.log(mutedTabs.keys());
-    console.log(updatedTab);
-    var audible = updatedTab.audible;
-    var notMuted = updatedTab.mutedInfo;
-  
-    if (notMuted && audible){ 
-      console.log("Muting tab: ".concat(updatedTab.title));
-      chrome.tabs.update(updatedTab.id, {"muted": true});
-      mutedTabs.set(updatedTab.id, getBaseUrl(updatedTab.url));
-    } // muted tab has changed to new base url  
-    else if (mutedTabs.has(updatedTab.id) && (mutedTabs.get(updatedTab.id) !== getBaseUrl(updatedTab.url))) { 
-      if (audible) { 
+  chrome.storage.sync.get(["trustedSites"], function(result) {
+    chrome.tabs.get(updatedTabId, function (updatedTab) {
+      var trustedSites = result.trustedSites
+      var audible = updatedTab.audible;
+      var notMuted = updatedTab.mutedInfo;
+    
+      if (notMuted && audible && !trustedSites.includes(updatedTab.url)){ 
         console.log("Muting tab: ".concat(updatedTab.title));
+        chrome.tabs.update(updatedTab.id, {"muted": true});
         mutedTabs.set(updatedTab.id, getBaseUrl(updatedTab.url));
-      } else { // unmute and delete from map 
-        console.log("unmuting tab: " + updatedTab.id);
-        chrome.tabs.update(updatedTab.id, {"muted": false});
-        console.log(mutedTabs.delete(updatedTab.id));
+      } // muted tab has changed to new base url  
+      else if (mutedTabs.has(updatedTab.id) && (mutedTabs.get(updatedTab.id) !== getBaseUrl(updatedTab.url))) { 
+        if (audible && !trustedSites.includes(updatedTab.url)) { 
+          console.log("Muting tab: ".concat(updatedTab.title));
+          mutedTabs.set(updatedTab.id, getBaseUrl(updatedTab.url));
+        } else { // unmute and delete from map 
+          console.log("Unmuting tab: " + updatedTab.id);
+          chrome.tabs.update(updatedTab.id, {"muted": false});
+          console.log(mutedTabs.delete(updatedTab.id));
+        }
       }
-      
-    }
+    });
   });
 });
 
--- options.js
+++ options.js
@@ -1,5 +1,6 @@
 trustedSitesElement = document.querySelector("#trusted-sites-list");
 saveButtonElement = document.querySelector("#save-trusted-sites");
+clearButtonElement = document.querySelector("#clear-trusted-sites");
 
 function fillInitial() {
     chrome.storage.sync.get(['trustedSites'], function(result) {
@@ -14,12 +15,16 @@ function saveTrusted(){
     chrome.storage.sync.set({trustedSites: newTrustedSites}, function() {
         console.log("Sucessfully set trusted sites.");
       });
-
-
     chrome.storage.sync.get(['trustedSites'], function(result) {
         console.log(result);
     });
 }
 
+function clearTrusted(){
+    chrome.storage.sync.clear(() => console.log("Cleared Trusted Sites"));
+    trustedSitesElement.value = trustedSitesStr;
+}
+
 fillInitial();
 saveButtonElement.addEventListener("click", saveTrusted);
+clearButtonElement.addEventListener("click", clearTrusted)
